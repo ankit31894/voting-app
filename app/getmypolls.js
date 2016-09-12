c2b9b327723cd226e51658dc2ced8d7e6dad9b3e
Ankit <ankit31894@gmail.com>
2016-09-12 17:39:17 +0530

    Fixed mypolls template

--- app/getmypolls.js
+++ app/getmypolls.js
@@ -3,7 +3,17 @@ module.exports={
     getAll:function(req,res,next){
         Poll.find({userId:req.user.id},function(err,polls){
             if(err)return next(err);
-            res.json(polls);
+            var npolls=[];
+            polls.forEach(function(poll){
+                var temp=poll.countVotes();
+                temp.noOfOptions=temp.options.length;
+                temp.noOfVotes=(temp.options.length===0?0:temp.options.reduce(function(p,c){
+                    return p+c.votes;
+                },0));
+                delete temp.options;
+                npolls.push(temp);
+            })
+            res.json(npolls);
         });
     }
 }
