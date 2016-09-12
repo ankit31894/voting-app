5ec1101236a54fc5edf28d1c212093b2a7e43fd4
Ankit <ankit31894@gmail.com>
2016-09-12 12:20:03 +0530

    Not changing callbak to https in google

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '186898052110-87psit9eeb62n0s72kgioo1bscvf1vev.apps.googleusercontent.com',
         'clientSecret'  : 'hAZMJ1u7bfsbsZYW6kOvmmv6',
-        'callbackURL'   : '/login/google/callback'
+        'callbackURL'   : 'https://ankit31894-va.herokuapp.com/login/google/callback'
     }
 
 };
